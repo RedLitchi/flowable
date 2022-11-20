f5652941d24be6ca25f9d7cb4535bd970aa247d1
猿陆柒 <2548841623@qq.com>
2022-11-20 22:58:16 +0800

    初始化项目

---
+++ src/main/java/top/yuan67/webapp/FlowableApplication.java
@@ -0,0 +1,17 @@
+/**
+ * @Author: NieQiang
+ * @User: CAPTAIN
+ * @CreateTime: 2022-11-20
+ * @Desc: 描述信息
+ **/
+package top.yuan67.webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowableApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(FlowableApplication.class, args);
+  }
+}
